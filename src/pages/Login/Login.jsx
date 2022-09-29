0fb5a1897a188df9decf92eccc454b8e5ee9d545
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-09-29 21:24:33 +0300

    add api response handling and ui for it

--- src/pages/Login/Login.jsx
+++ src/pages/Login/Login.jsx
@@ -1,4 +1,4 @@
-import { Button, Dot, Input, Note, Spacer, Text, useToasts } from '@geist-ui/core';
+import { Button, Dot, Input, Text, useToasts } from '@geist-ui/core';
 import { Lock, User } from '@geist-ui/icons';
 import { useState } from 'react';
 import { Link } from 'react-router-dom';
@@ -9,14 +9,18 @@ const Login = () => {
   const { setToast } = useToasts();
   const [login, setLogin] = useState('');
   const [password, setPassword] = useState('');
-  const [loginStatus, setLoginStatus] = useState({success: true, message: ''});
-  const [passwordtatus, setPasswordStatus] = useState({success: true, message: ''});
+  const [loginStatus, setLoginStatus] = useState({ type: '', message: '', showNotify: false, notifyType: '' });
+  const [passwordStatus, setPasswordStatus] = useState({ type: '', message: '', showNotify: false, notifyType: '' });
+  const [loading, setLoading] = useState(false);
 
   const handleLoginChange = (e) => {
     setLogin(e.target.value);
+    setLoginStatus((prevVal) => ({ ...prevVal, type: '' }));
   };
+  
   const handlePasswordChange = (e) => {
     setPassword(e.target.value);
+    setPasswordStatus((prevVal) => ({ ...prevVal, type: '' }));
   };
 
   const handleSubmit = async (e) => {
@@ -24,76 +28,106 @@ const Login = () => {
     const data = { login, password };
 
     try {
-      // const response = await axios.post(`${process.env.REACT_APP_API_URL}auth/login`, data);
+      setLoading(true);
       const response = await api.post('auth/login', data);
+      setLoading(false);
 
       setLoginStatus(response.data);
       setPasswordStatus(response.data);
-      console.log(response);
 
+      const inputStatus = {
+        type: response.data.success ? '' : 'error',
+        message: response.data.message,
+        showNotify: !response.data.success,
+        notifyType: response.data.success ? '' : 'error',
+      };
 
+      setLoginStatus(inputStatus);
+      setPasswordStatus(inputStatus);
     } catch (error) {
+      setLoading(false);
       setToast({
-        text: 'Network Error',
+        text: error.message,
         type: 'error',
       });
       console.error(error);
     }
   };
 
+  const notyfication = (showNotify, notifyType, message) => (
+    <span className={showNotify ? '' : s.hide}>
+      <Dot type={notifyType} />
+      <Text small type={notifyType}>
+        {message}
+      </Text>
+    </span>
+  );
+
   return (
     <div className={s.wrapper}>
       <div className={s.container}>
         <div className={s.logo}>
-          <Link to={'/'}>Logo</Link>
+          <Link to={'/'}>
+            Logo
+          </Link>
         </div>
         <div className={s.content}>
-          <Text h3 className={s.title}>
+          <Text h2 font="22px" className={s.title}>
             Log in to your account
           </Text>
-          {/* <Note scale={0.7}  type="error" label="error" filled>Login or password is invalid</Note>
-          <br/> */}
-          <form onSubmit={handleSubmit}>
+          <form className={s.form} onSubmit={handleSubmit}>
             <Input
+              id="login"
               name="login"
-              icon={<User color={loginStatus.success ? '' : 'red'} />}
-              type={loginStatus.success ? 'default' : 'error'}
-              scale={1.2}
-              width="100%"
+              icon={<User color={loginStatus.type ? 'red' : ''} />}
+              type={loginStatus.type ? 'error' : 'default'}
+              scale={1.1}
               placeholder="Login"
+              value={login}
               onChange={handleLoginChange}
+              mb="15px"
+              w="100%"
             >
-            <div>
-              <Dot type={loginStatus.success ? '' : 'error'} style={{ marginLeft: '10px' }}/>
-                <Text small type="error">
-                  {loginStatus.message}
-                </Text>
-              {/* </Dot> */}
-            </div>
+              {/* <Text b font="14px" ml="5px">
+                Login
+              </Text> */}
+              <label htmlFor="login" className={s.label}>
+                Login
+              </label>
+              {notyfication(loginStatus.showNotify, loginStatus.notifyType, loginStatus.message)}
             </Input>
-            <Spacer h={2} />
+
             <Input.Password
+              id="password"
               name="password"
-              icon={<Lock color={loginStatus.success ? '' : 'red'} />}
-              type={loginStatus.success ? 'secondary' : 'error'}
-              scale={1.2}
-              width="100%"
+              icon={<Lock color={passwordStatus.type ? 'red' : ''} />}
+              type={passwordStatus.type ? 'error' : 'default'}
+              scale={1.1}
               placeholder="Password"
+              value={password}
               onChange={handlePasswordChange}
+              w="100%"
             >
-              <div>
-              <Dot type="error" style={{ marginLeft: '10px' }}/>
-                <Text small type="error">
-                  Login or password is invalid
-                </Text>
-              {/* </Dot> */}
-            </div>
+              {/* <Text b font="14px" ml="5px">
+                Password
+              </Text> */}
+              <label htmlFor="password" className={s.label}>
+                Password
+              </label>
+              {notyfication(passwordStatus.showNotify, passwordStatus.notifyType, passwordStatus.message)}
             </Input.Password>
 
-            <Button type="secondary" htmlType="submit">
-              Login
+            <Button loading={loading} type="secondary-light" htmlType="submit" mt="30px" w="100%">
+              Log in
             </Button>
           </form>
+
+          <Text span type="secondary">
+            Don't have an account yeat?{' '}
+            <Link className={s.registerLink} to="/register" >
+              Sign up
+            </Link>
+          </Text>
         </div>
       </div>
     </div>
