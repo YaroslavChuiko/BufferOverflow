bcf73c352664f82a61da74703b04ab4d61216367
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-09-29 21:25:29 +0300

    add /register route

--- src/App.jsx
+++ src/App.jsx
@@ -1,8 +1,7 @@
-import { Card } from '@geist-ui/core';
 import { Route, Routes } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
-import Header from './shared/Header/Header';
+import Register from './pages/Register/Register';
 import PageLayout from './shared/PageLayout/PageLayout';
 import './styles/index.scss';
 
@@ -11,6 +10,7 @@ const App = () => {
     <>
       <Routes>
         <Route path="/login" element={<Login />} />
+        <Route path="/register" element={<Register />} />
         <Route element={<PageLayout />}>
           <Route path="/" element={<Home />} />
           <Route path="/profile" element={<div>profile</div>} />
