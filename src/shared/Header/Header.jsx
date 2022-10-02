92570135083ff810e1aa9e7849490ac1ce855a29
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-02 20:06:56 +0300

    add user optional rendering

--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,12 +1,14 @@
-import React from 'react';
+import { Avatar, Button } from '@geist-ui/core';
+import { useSelector } from 'react-redux';
 import { Link, NavLink, useNavigate } from 'react-router-dom';
-import { Avatar, Button, User } from '@geist-ui/core';
+import { selectUser } from '../../store/selectors';
 import Container from '../Container/Container';
 
 import s from './Header.module.scss';
 
 const Header = () => {
   const navigate = useNavigate();
+  const { loggedIn, userData } = useSelector(selectUser);
 
   return (
     <header className={s.header}>
@@ -27,6 +29,29 @@ const Header = () => {
             </NavLink>
           </nav>
 
+          {loggedIn ? (
+            <div className={s.user}>
+              <Link to={'/profile'}>
+                <Avatar scale={1.5} src={`${process.env.REACT_APP_API_URL}images/${userData.avatar}`} text="YC" />
+              </Link>
+              <div className={s.userInfo}>
+                <span className={s.userName}>{userData.login}</span>
+                <span className={s.userRole}>{userData.role}</span>
+              </div>
+            </div>
+          ) : (
+            <Button
+              scale={0.8}
+              auto
+              type="secondary"
+              onClick={() => {
+                navigate('/login');
+              }}
+            >
+              Sign in
+            </Button>
+          )}
+
           {/* //! add color default */}
           {/* <div className={s.user}>
             <Link to={'/profile'}> 
@@ -37,7 +62,7 @@ const Header = () => {
               <span className={s.userRole}>user</span>
             </div>
           </div> */}
-          <Button scale={0.8} auto type='secondary' onClick={() => {navigate("/login")}}>Sign in</Button>
+          {/* <Button scale={0.8} auto type='secondary' onClick={() => {navigate("/login")}}>Sign in</Button> */}
 
           {/* <User src={`${process.env.REACT_APP_API_URL}images/defaultAvatar/defaultAvatar.jpg`} name="ychuiko">
             user
