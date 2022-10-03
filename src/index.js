e1a3eb0a9b4e9e58967959e0abd94daacf981083
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-03 21:36:36 +0300

    dispatch isLoggedIn

--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import React from 'react';
 import { createRoot } from 'react-dom/client';
 import { Provider } from 'react-redux';
 import { BrowserRouter } from 'react-router-dom';
-import { store } from './store/store';
 import App from './App';
+import { store } from './store/store';
+import { isLoggedIn } from './store/thunks/userThunk';
 
 const container = document.getElementById('app');
 const root = createRoot(container);
+
+store.dispatch(isLoggedIn());
 
 root.render(
   <React.StrictMode>
