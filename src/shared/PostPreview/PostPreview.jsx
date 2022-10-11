436840d9c84930a7d87ba71e836281a76576d95e
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-11 13:28:33 +0300

    update PostPreview

--- src/shared/PostPreview/PostPreview.jsx
+++ src/shared/PostPreview/PostPreview.jsx
@@ -1,23 +1,21 @@
-import { Avatar, Card, Tag } from '@geist-ui/core';
-import React, { useEffect, useState } from 'react';
-import api from '../../lib/axios';
+import { Avatar, Card, Tag, Text } from '@geist-ui/core';
+import moment from 'moment';
+import { Link } from 'react-router-dom';
 import { useGetPostAuthorQuery, useGetPostCategoriesQuery } from '../../store/api/apiSlice';
-
+import capitalizeFirstLetter from '../../utils/capitalizeFirstLetter';
+import Vote from '../Vote/Vote';
 import s from './PostPreview.module.scss';
 
 const PostPreview = ({ post }) => {
-  // const [author, setAuthor] = useState([]);
-  // const [categories, setCategories] = useState([]);
-  // const [likes, setlikes] = useState([]);
-
   const params = new URLSearchParams();
   post.post_categories.forEach((value) => params.append('id', value));
+
   const {
     data: categories,
-    isCategoriesLoading,
-    isCategoriesSuccess,
-    isCategoriesError,
-    categoriesError,
+    isLoading: isCategoriesLoading,
+    isSuccess: isCategoriesSuccess,
+    isError: isCategoriesError,
+    error: categoriesError,
   } = useGetPostCategoriesQuery(`?${params.toString()}`);
 
   const {
@@ -28,44 +26,15 @@ const PostPreview = ({ post }) => {
     error: authorError,
   } = useGetPostAuthorQuery(post.author_id);
 
-  //! get user insode post or separete query (categories)
-  //! get all post alikes or only one
-  // useEffect(() => {
-  //   Promise.all([getPostAuthor(), getPostCategories(), getPostLikes()]).then(function (results) {
-  //     // const author = results[0].data;
-  //     setAuthor(results[0].data);
-  //     // const categories = results[1].data;
-  //     setCategories(results[1].data);
-  //     const likes = results[2].data;
-  //     console.log('author', author);
-  //     console.log('categories', categories);
-  //     console.log('likes', likes);
-  //   });
-  // }, []);
-
-  // const getPostAuthor = () => {
-  //   return api.get(`users/${post.author_id}`);
-  // };
-
-  // const getPostCategories = () => {
-  //   const params = new URLSearchParams();
-  //   post.post_categories_id.forEach((value) => params.append('id', value));
-  //   console.log(params.toString());
-
-  //   return api.get(`categories?${params.toString()}`);
-  // };
-
-  // const getPostLikes = () => {
-  //   return api.get(`likes?target_post=${post.id}`);
-  // };
-
-  // console.log(author);
-
   return (
     <Card mb="10px">
-      <h3>{post.title}</h3>
+      <h3 className={s.title}>
+        <Link className={s.titleLink} to={`/question/${post.id}`}>
+          {capitalizeFirstLetter(post.title)}
+        </Link>
+      </h3>
       <p>{post.content}</p>
-      <div>
+      <div className={s.categories}>
         {isCategoriesSuccess &&
           categories.map((item) => (
             <Tag key={item.id} type="lite" mr="10px">
@@ -73,16 +42,27 @@ const PostPreview = ({ post }) => {
             </Tag>
           ))}
       </div>
-      <div>
-        by
-        {isAuthorSuccess && (
-          <>
-            <Avatar src={`${process.env.REACT_APP_GET_IMG_BASEURL}${author?.profile_picture}`} ml="5px" mr="5px" />
-            <span>{author.login}</span>
-          </>
-        )}
-        <span> about {post.publish_date} ago</span>
-        <span> rating: {post.rating}</span>
+      <div className={s.footer}>
+        <div className={s.info}>
+          <div className={s.user}>
+            by
+            {isAuthorSuccess && (
+              <>
+                <Avatar src={`${process.env.REACT_APP_GET_IMG_BASEURL}${author?.profile_picture}`} ml="5px" mr="5px" />
+                <Text span type="success">
+                  {author.login}
+                </Text>
+              </>
+            )}
+          </div>
+
+          <div className={s.date} title={post.publish_date}>
+            {moment(post.publish_date).fromNow()}
+          </div>
+        </div>
+        <div className={s.rating}>
+          <Vote postId={post.id} voteCount={post.rating} />
+        </div>
       </div>
     </Card>
   );
