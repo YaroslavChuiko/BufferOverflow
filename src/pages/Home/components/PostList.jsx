83e183676ee8e4ef5dcdb4a023d17a4890311590
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-12 14:06:03 +0300

    add params prop

--- src/pages/Home/components/PostList.jsx
+++ src/pages/Home/components/PostList.jsx
@@ -1,46 +1,39 @@
-import { Button, ButtonGroup, Card, Loading } from '@geist-ui/core';
-import React, { useEffect, useState } from 'react';
+import { Loading } from '@geist-ui/core';
+import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
-import api from '../../../lib/axios';
+import { useSearchParams } from 'react-router-dom';
 import Pagination from '../../../shared/Pagination/Pagination';
 import PostPreview from '../../../shared/PostPreview/PostPreview';
 import SelectPageSize from '../../../shared/SelectPageSize/SelectPageSize';
 import { useGetPostsQuery } from '../../../store/api/apiSlice';
 import { selectUser } from '../../../store/selectors';
-import { useSearchParams } from 'react-router-dom';
-// import { usePagination } from "react-use-pagination";
 
 import s from './PostList.module.scss';
 
-const PostList = ({}) => {
+const PostList = ({ params }) => {
   let [searchParams, setSearchParams] = useSearchParams();
-  const [page, setPage] = useState(+searchParams.get('page') || 1); //! add page number as page query param
+  const [page, setPage] = useState(1);
   const { pageSize } = useSelector(selectUser);
+
+  useEffect(() => {
+    // const page = +searchParams.get('page');
+    // setSearchParams((prev) => ({ sort: prev.get('sort'), page: page }));
+    setPage(1);
+  }, [pageSize]);
 
   const param = new URLSearchParams();
   param.append('_start', String(pageSize * page - pageSize));
   param.append('_end', String(pageSize * page));
-  param.append('_order', 'DESC');
-  param.append('_sort', 'rating');
-
+  for (const key in params) {
+    param.append(key, params[key]);
+  }
   const { data, isLoading, isFetching, isSuccess, isError, error } = useGetPostsQuery(`?${param.toString()}`);
-
-  // useEffect(() => {
-  //   api.get('posts?_order=DESC&_sort=rating')
-  //   .then(response => {
-  //     console.log('X-Total-Count', response.headers['x-total-count']);
-  //     setPosts(response.data)
-  //   })
-  // }, [])
 
   const handlePageChange = (val) => {
     // searchParams.get('sort')
-    setSearchParams((prev) => ({ sort: prev.get('sort'), page: val }));
+    // setSearchParams((prev) => ({ sort: prev.get('sort'), page: val }));
     setPage(val);
   };
-  // console.log(data);
-
-  // let content;
 
   if (isLoading) {
     return <Loading>Loading</Loading>;
