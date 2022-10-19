1eb4e39034c16117c803e13409bdacff30a3426d
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-19 23:25:00 +0300

    add CommentList component and styles for it

---
+++ src/pages/Post/components/CommentList.jsx
@@ -0,0 +1,74 @@
+import { Tooltip } from '@geist-ui/core';
+import { useState } from 'react';
+import { useSelector } from 'react-redux';
+import { Link } from 'react-router-dom';
+import { useGetAnswerCommentsQuery } from '../../../store/api/apiSlice';
+import { selectUser } from '../../../store/selectors';
+import Comment from './Comment';
+import CommentEditor from './CommentEditor';
+import s from './CommentList.module.scss';
+
+const CommentList = ({ answerId }) => {
+  const { loggedIn, userData } = useSelector(selectUser);
+  const [showComments, setShowComments] = useState(false);
+
+  const {
+    data: comments,
+    isLoading: isCommentsLoading,
+    isSuccess: isCommentsSuccess,
+    isError: isCommentsErrod,
+    error: commentsError,
+  } = useGetAnswerCommentsQuery(answerId);
+
+  const handleShowCommentsClick = (e) => {
+    e.preventDefault();
+    setShowComments((prevVal) => !prevVal);
+  };
+
+  let showCommentsBtnText;
+
+  if (showComments) {
+    showCommentsBtnText = 'Hide comments';
+  } else if (isCommentsSuccess && comments.totalCount == 0) {
+    showCommentsBtnText = `Add comment`;
+  } else if (isCommentsSuccess && comments.totalCount == 1) {
+    showCommentsBtnText = `Show 1 comment`;
+  } else if (isCommentsSuccess && comments.totalCount > 1) {
+    showCommentsBtnText = `Show ${comments.totalCount} comments`;
+  }
+
+  const showCommentsBtn = (
+    <button className={s.button} onClick={handleShowCommentsClick}>
+      {showCommentsBtnText}
+    </button>
+  );
+  
+  return (
+    <div className={s.commentList}>
+      <div className={s.control}>{showCommentsBtn}</div>
+      {showComments && (
+        <div className={s.comments}>
+          {isCommentsSuccess && comments.totalCount > 0 && (
+            <div className={s.content}>
+              {comments.comments.map((comment) => (
+                <Comment key={comment.id} comment={comment} />
+              ))}
+            </div>
+          )}
+          <div className={s.addComment}>
+            {loggedIn ? (
+              <CommentEditor answerId={answerId} />
+            ) : (
+              <div className={s.notLoggedIn}>
+                <Link to={'/login'}>Login</Link>
+                <span>to leave a comment</span>
+              </div>
+            )}
+          </div>
+        </div>
+      )}
+    </div>
+  );
+};
+
+export default CommentList;
