48fe8543e8b806922b84a477b3b615a5aaac41c5
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-20 23:05:03 +0300

    rename

--- src/pages/Post/components/CommentCreate.jsx
+++ src/pages/Post/components/CommentCreate.jsx
@@ -1,17 +1,17 @@
 import { Button, Text, Textarea } from '@geist-ui/core';
 import { useState } from 'react';
 import { useSelector } from 'react-redux';
-import { useAddNewCommentMutation } from '../../../store/api/apiSlice';
+import { useCreateCommentMutation } from '../../../store/api/commentSlice';
 import { selectUser } from '../../../store/selectors';
 import { validateLength } from '../../../validation/richTextValidation';
-import s from './CommentEditor.module.scss';
+import s from './CommentCreate.module.scss';
 
-const CommentEditor = ({ answerId, initialValue = '' }) => {
+const CommentCreate = ({ answerId, initialValue = '', mode ='create' }) => {
   const { loggedIn, userData } = useSelector(selectUser);
   const [editorValue, setEditorValue] = useState(initialValue);
   const [notify, setNotify] = useState({ type: 'default', message: '' });
 
-  const [addNewComment, { isLoading, isSuccess }] = useAddNewCommentMutation();
+  const [createComment, { isLoading, isSuccess }] = useCreateCommentMutation();
 
   const handleTextareaChange = (e) => {
     setEditorValue(e.target.value);
@@ -35,7 +35,7 @@ const CommentEditor = ({ answerId, initialValue = '' }) => {
       status: 'active',
     };
     try {
-      await addNewComment(newComment).unwrap();
+      await createComment(newComment).unwrap();
       setEditorValue('');
       setNotify({ type: 'success', message: 'Successfully posted.' });
     } catch (error) {
@@ -48,7 +48,7 @@ const CommentEditor = ({ answerId, initialValue = '' }) => {
   };
 
   return (
-    <div className={s.commentEditor}>
+    <div className={s.commentCreate}>
       <form onSubmit={handleSubmit}>
         <Textarea
           value={editorValue}
@@ -58,13 +58,11 @@ const CommentEditor = ({ answerId, initialValue = '' }) => {
           width="100%"
           height="100px"
         />
-        {/* {notify && ( */}
         <div className={s.notify}>
           <Text span type={notify.type}>
             {notify.message}
           </Text>
         </div>
-        {/* )} */}
         <Button loading={isLoading} auto type="success-light" ghost font="14px" scale={0.7} htmlType="submit">
           Post your comment
         </Button>
@@ -73,4 +71,4 @@ const CommentEditor = ({ answerId, initialValue = '' }) => {
   );
 };
 
-export default CommentEditor;
+export default CommentCreate;
