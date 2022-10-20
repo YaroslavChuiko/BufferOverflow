7f0f12b54857b8fac774ff86d0b9f595d3ee17b9
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-20 23:17:30 +0300

    split

--- src/store/api/apiSlice.js
+++ src/store/api/apiSlice.js
@@ -17,68 +17,13 @@ export const apiSlice = createApi({
     }),
     getPost: builder.query({
       query: (postId) => `/posts/${postId}`,
-      providesTags: (result, error, arg) => [{type: 'Post', id: arg}],
+      providesTags: (result, error, arg) => [{ type: 'Post', id: arg }],
     }),
     getAuthor: builder.query({
       query: (userId) => `/users/${userId}`,
     }),
     getPostCategories: builder.query({
       query: (queryParams) => `/categories${queryParams}`,
-    }),
-    getPostAnswers: builder.query({
-      query: (queryParams) => `/answers${queryParams}`,
-      transformResponse(answers, meta) {
-        return { answers, totalCount: Number(meta.response.headers.get('X-Total-Count')) };
-      },
-      providesTags: (result, error, arg) => {
-        const answers = result?.answers || [];
-        return ['Answer', ...answers.map(({ id }) => ({ type: 'Answer', id }))];
-      },
-    }),
-    addNewAnswer: builder.mutation({
-      query: ({ author_id, post_id, content, status }) => ({
-        url: `/answers`,
-        method: 'POST',
-        body: { author_id, post_id, content, status },
-      }),
-      invalidatesTags: ['Answer'],
-    }),
-    deleteAnswer: builder.mutation({
-      query: (answerId) => ({
-        url: `/answers/${answerId}`,
-        method: 'DELETE',
-        body: {},
-      }),
-      invalidatesTags: ['Answer'],
-    }),
-    getAnswerComments: builder.query({
-      query: (answerId) => `/comments?_order=ASC&_sort=publish_date&status=active&answer_id=${answerId}`,
-      transformResponse(comments, meta) {
-        return { comments, totalCount: Number(meta.response.headers.get('X-Total-Count')) };
-      },
-      providesTags: (result, error, arg) => {
-        const comments = result?.comments || [];
-        return ['Comment', ...comments.map(({ id }) => ({ type: 'Comment', id }))];
-      },
-    }),
-    addNewComment: builder.mutation({
-      query: ({ author_id, answer_id, content, status }) => ({
-        url: `/comments`,
-        method: 'POST',
-        body: { author_id, answer_id, content, status },
-      }),
-      invalidatesTags: ['Comment'],
-    }),
-    deleteComment: builder.mutation({
-      query: (commentId) => ({
-        url: `/comments/${commentId}`,
-        method: 'DELETE',
-        body: {},
-      }),
-      invalidatesTags: ['Comment'],
-    }),
-    checkPostLike: builder.query({
-      query: ({target, id}) => `/${target}/${id}/checkLike`,
     }),
     getCategories: builder.query({
       query: (q) => `/categories?_end=10&_order=ASC&_sort=title&_start=0&q=${q}`,
@@ -89,6 +34,8 @@ export const apiSlice = createApi({
   }),
 });
 
+// ! add logout and invalidate all tags
+
 export const {
   useGetPostsQuery,
   useGetPostQuery,
@@ -96,14 +43,5 @@ export const {
   useLazyGetAuthorQuery,
   useGetPostCategoriesQuery,
   useLazyGetPostCategoriesQuery,
-  useCheckPostLikeQuery,
   useLazyGetCategoriesQuery,
-
-  useGetPostAnswersQuery,
-  useAddNewAnswerMutation,
-  useDeleteAnswerMutation,
-
-  useGetAnswerCommentsQuery,
-  useAddNewCommentMutation,
-  useDeleteCommentMutation,
 } = apiSlice;
