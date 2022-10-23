6dfe9dff0875cd37e44c92cef418a03741d1e833
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-23 22:26:19 +0300

    add /post/:postId/edit path

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Post from './pages/Post/Post';
 import PostCreate from './pages/PostCreate/PostCreate';
+import PostEdit from './pages/PostEdit/PostEdit';
 import Register from './pages/Register/Register';
 import PageLayout from './shared/PageLayout/PageLayout';
 import ProtectedRoute from './shared/ProtectedRoute/ProtectedRoute';
@@ -15,10 +16,16 @@ const App = () => {
         <Route path="/login" element={<Login />} />
         <Route path="/register" element={<Register />} />
         <Route element={<PageLayout />}>
-        <Route path="/create" element={<PostCreate />} /> //!
+
+          <Route path="/create" element={<PostCreate />} /> //!
+          <Route path="edit" element={<PostEdit />} />
+          
           <Route path="/" element={<Home />} />
-          <Route path="/post" >
+          <Route path="/post">
             <Route path=":postId" element={<Post />} />
+            <Route element={<ProtectedRoute />}>
+              <Route path=":postId/edit" element={<PostEdit />} />
+            </Route>
             <Route element={<ProtectedRoute />}>
               <Route path="create" element={<PostCreate />} />
             </Route>
