cb3ffd089412c2af1109f7f71fec1b9c74d6b80e
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-23 22:20:57 +0300

    update getText

--- src/utils/getText.js
+++ src/utils/getText.js
@@ -1,3 +1,4 @@
-const getText = (html) => html.replace(/<[^>]+>/g, '');
+// delete tags, &nbsp; &lt; ..., \n
+const getText = (html) => html.replace(/<[^>]+>/g, ' ').replace(/\&[a-z]+;|\n/g, ' '); 
 
 export default getText;
