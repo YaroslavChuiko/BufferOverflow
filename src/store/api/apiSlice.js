8869c7631bc08820dd03d0272ce5c9bf97b79d73
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-24 22:59:54 +0300

    move post actions to postSlice, add deleteUser action

--- src/store/api/apiSlice.js
+++ src/store/api/apiSlice.js
@@ -5,47 +5,6 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
   tagTypes: ['Post', 'Answer', 'Comment'],
   endpoints: (builder) => ({
-    getPosts: builder.query({
-      query: (queryParams) => `/posts${queryParams}`,
-      transformResponse(posts, meta) {
-        return { posts, totalCount: Number(meta.response.headers.get('X-Total-Count')) };
-      },
-      providesTags: (result, error, arg) => {
-        const posts = result?.posts || [];
-        return ['Post', ...posts.map(({ id }) => ({ type: 'Post', id }))];
-      },
-    }),
-    getPost: builder.query({
-      query: (postId) => `/posts/${postId}`,
-      providesTags: (result, error, arg) => [{ type: 'Post', id: arg }],
-    }),
-    createPost: builder.mutation({
-      query: ({ author_id, title, content, post_categories, status}) => ({
-        url: `/posts`,
-        method: 'POST',
-        body: { author_id, title, content, post_categories, status},
-      }),
-      invalidatesTags: ['Post'],
-    }),
-    updatePost: builder.mutation({
-      query: ({ id, author_id, title, content, post_categories, status }) => ({
-        url: `/posts/${id}`,
-        method: 'PUT',
-        body: { author_id, title, content, post_categories, status },
-      }),
-      invalidatesTags: (result, error, arg) => [{ type: 'Post', id: arg.id }],
-    }),
-    deletePost: builder.mutation({
-      query: (postId) => ({
-        url: `/posts/${postId}`,
-        method: 'DELETE',
-        body: {},
-      }),
-      invalidatesTags: ['Post'],
-    }),
-    getPostCategories: builder.query({
-      query: (queryParams) => `/categories${queryParams}`,
-    }),
     getAuthor: builder.query({
       query: (userId) => `/users/${userId}`,
     }),
@@ -55,20 +14,21 @@ export const apiSlice = createApi({
         return categories.map((item) => ({ id: item.id, label: item.title, value: item.title }));
       },
     }),
+    deleteUser: builder.mutation({
+      query: (userId) => ({
+        url: `/users/${userId}`,
+        method: 'DELETE',
+        body: {},
+      }),
+    }),
   }),
 });
 
 // ! add logout and invalidate all tags
 
 export const {
-  useGetPostsQuery,
-  useGetPostQuery,
-  useCreatePostMutation,
-  useUpdatePostMutation,
-  useDeletePostMutation,
   useGetAuthorQuery,
   useLazyGetAuthorQuery,
-  useGetPostCategoriesQuery,
-  useLazyGetPostCategoriesQuery,
   useLazyGetCategoriesQuery,
+  useDeleteUserMutation,
 } = apiSlice;
