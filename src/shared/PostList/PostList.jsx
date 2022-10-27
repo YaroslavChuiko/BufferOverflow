dfd500d31d8eaecf4cc8ff5a5e6970feab1e797e
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-27 23:01:36 +0300

    move

--- src/shared/PostList/PostList.jsx
+++ src/shared/PostList/PostList.jsx
@@ -2,11 +2,11 @@ import { Button, Loading } from '@geist-ui/core';
 import { useEffect, useState } from 'react';
 import { useSelector } from 'react-redux';
 import { useNavigate, useSearchParams } from 'react-router-dom';
-import Pagination from '../../../shared/Pagination/Pagination';
-import PostPreview from '../../../shared/PostPreview/PostPreview';
-import SelectPageSize from '../../../shared/SelectPageSize/SelectPageSize';
-import { useGetPostsQuery } from '../../../store/api/postSlice';
-import { selectUser } from '../../../store/selectors';
+import Pagination from '../Pagination/Pagination';
+import PostPreview from '../PostPreview/PostPreview';
+import SelectPageSize from '../SelectPageSize/SelectPageSize';
+import { useGetPostsQuery } from '../../store/api/postSlice';
+import { selectUser } from '../../store/selectors';
 
 import s from './PostList.module.scss';
 
@@ -29,15 +29,13 @@ const PostList = ({ params, filter }) => {
   if (filter) {
     param.append('post_categories', filter);
   }
-  // post_categories=1,
+  
   for (const key in params) {
     param.append(key, params[key]);
   }
   const { data, isLoading, isFetching, isSuccess, isError, error } = useGetPostsQuery(`?${param.toString()}`);
 
   const handlePageChange = (val) => {
-    // searchParams.get('sort')
-    // setSearchParams((prev) => ({ sort: prev.get('sort'), page: val }));
     setPage(val);
   };
 
@@ -58,9 +56,6 @@ const PostList = ({ params, filter }) => {
             <Button type="success-light" onClick={(e) => navigate('/post/create')}>Ask question</Button>
           </div>
         )}
-        {/* {data?.posts.map((post) => (
-          <PostPreview key={post.id} post={post} />
-        ))} */}
       </div>
       <div className={s.pageControls}>
         <Pagination count={Math.ceil(data.totalCount / pageSize)} limit={5} page={page} onChange={handlePageChange} />
