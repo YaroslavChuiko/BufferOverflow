8add997f397ca2036c20caf30be909926d9607f3
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-29 14:45:20 +0300

    refactor

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
 import { Route, Routes } from 'react-router-dom';
-import Home from './pages/Home/Home';
 import Login from './pages/Login/Login';
 import Post from './pages/Post/Post';
 import PostCreate from './pages/PostCreate/PostCreate';
 import PostEdit from './pages/PostEdit/PostEdit';
 import Profile from './pages/Profile/Profile';
 import ProfileEdit from './pages/ProfileEdit/ProfileEdit';
+import Questions from './pages/Questions/Questions';
 import Register from './pages/Register/Register';
 import ResetPassword from './pages/ResetPassword/ResetPassword';
 import PageLayout from './shared/PageLayout/PageLayout';
@@ -17,11 +17,11 @@ const App = () => {
     <>
       <Routes>
         <Route element={<PageLayout />}>
+          <Route path="/" element={<Questions />} />
+
           <Route path="/register" element={<Register />} />
           <Route path="/login" element={<Login />} />
           <Route path="/reset-password/:token" element={<ResetPassword />} />
-
-          <Route path="/" element={<Home />} />
 
           <Route path="/post/:postId" element={<Post />} />
           <Route element={<ProtectedRoute />}>
@@ -29,7 +29,6 @@ const App = () => {
             <Route path="/post/create" element={<PostCreate />} />
           </Route>
 
-          <Route path="/questions" element={<div>questions</div>} />
           <Route element={<ProtectedRoute />}>
             <Route path="/profile" element={<Profile />} />
             <Route path="/profile/edit" element={<ProfileEdit />} />
