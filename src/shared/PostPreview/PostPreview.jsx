62d49da5963d7d1fb51757ae86b4f05da9e4dc62
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-29 14:43:53 +0300

    refactor

--- src/shared/PostPreview/PostPreview.jsx
+++ src/shared/PostPreview/PostPreview.jsx
@@ -31,49 +31,48 @@ const PostPreview = ({ post }) => {
 
   return (
     <Card mb="10px">
-      <h3 className={s.title}>
-        <Link className={s.titleLink} to={`/post/${post.id}`}>
-          {capitalizeFirstLetter(post.title)}
-        </Link>
-      </h3>
-      <p>{truncate(getText(post.content), 250)}</p>
-      <div className={s.categories}>
-        {isCategoriesSuccess &&
-          categories.map((item) => (
-            <Tag key={item.id} type="lite" mr="10px">
-              {item.title}
-            </Tag>
-          ))}
-      </div>
-      <div className={s.footer}>
-        <div className={s.footerLeft}>
-          <div className={s.author}>
-            by
-            {isAuthorSuccess && (
-              <>
-                <Avatar src={`${process.env.REACT_APP_GET_IMG_BASEURL}${author?.profile_picture}`} ml="5px" mr="5px" />
-                <Text span type="success" title={`rating: ${author.rating}`}>
-                  {author.login}
-                </Text>
-              </>
-            )}
-          </div>
+      <div className={s.postPreview}>
+        <h3 className={s.title}>
+          <Link className={s.titleLink} to={`/post/${post.id}`}>
+            {capitalizeFirstLetter(post.title)}
+          </Link>
+        </h3>
+        <p>{truncate(getText(post.content), 250)}</p>
+        <div className={s.categories}>
+          {isCategoriesSuccess &&
+            categories.map((item) => (
+              <Tag key={item.id} type="lite" mr="10px">
+                {item.title}
+              </Tag>
+            ))}
+        </div>
+        <div className={s.footer}>
+          <div className={s.footerLeft}>
+            <div className={s.author}>
+              by
+              {isAuthorSuccess && (
+                <>
+                  <Avatar src={`${process.env.REACT_APP_GET_IMG_BASEURL}${author?.profile_picture}`} ml="5px" mr="5px" />
+                  <Text span type="success" title={`rating: ${author.rating}`}>
+                    {author.login}
+                  </Text>
+                </>
+              )}
+            </div>
 
-          <div className={s.date} title={post.publish_date}>
-            {moment(post.publish_date).fromNow()}
+            <div className={s.date} title={post.publish_date}>
+              {moment(post.publish_date).fromNow()}
+            </div>
+          </div>
+          <div className={s.footerRight}>
+            {post.status === 'inactive' && (
+              <Tag type="warning" scale={0.7} font="14px" mr="15px">
+                Status: Inactive
+              </Tag>
+            )}
+            <Vote postId={post.id} voteCount={post.rating} />
           </div>
         </div>
-        <div className={s.footerRight}>
-          {post.status === 'inactive' && (
-            <Tag type="warning" scale={0.7} font="14px" mr="15px">
-              Status: Inactive
-            </Tag>
-          )}
-          <Vote postId={post.id} voteCount={post.rating} />
-        </div>
-        {/* <div className={s.rating}>
-          <Vote postId={post.id} voteCount={post.rating} />
-        </div> */}
       </div>
     </Card>
   );
