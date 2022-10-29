8c0d2a39fea4d6524b4ef8c547f74425bd1f9b0b
YaroslavChuiko <jarik.tchuicko@gmail.com>
2022-10-29 23:16:30 +0300

    create authSlice

--- src/store/api/apiSlice.js
+++ src/store/api/apiSlice.js
@@ -5,14 +5,14 @@ export const apiSlice = createApi({
   baseQuery: fetchBaseQuery({ baseUrl: process.env.REACT_APP_API_URL }),
   tagTypes: ['Post', 'Answer', 'Comment', 'Like'],
   endpoints: (builder) => ({
-    getAuthor: builder.query({
-      query: (userId) => `/users/${userId}`,
-    }),
     getCategories: builder.query({
       query: (q) => `/categories?_end=10&_order=ASC&_sort=title&_start=0&q=${q}`,
       transformResponse(categories) {
         return categories.map((item) => ({ id: item.id, label: item.title, value: item.title }));
       },
+    }),
+    getAuthor: builder.query({
+      query: (userId) => `/users/${userId}`,
     }),
     updateUserData: builder.mutation({
       query: ({ id, login, full_name, profile_picture, role }) => ({
@@ -28,28 +28,6 @@ export const apiSlice = createApi({
         body: body,
       }),
     }),
-    register: builder.mutation({
-      query: ({ login, firstName, lastName, email, password, repassword }) => ({
-        url: `/auth/register`,
-        method: 'POST',
-        body: { login, firstName, lastName, email, password, repassword },
-      }),
-    }),
-    login: builder.mutation({
-      query: ({ login, password }) => ({
-        url: `/auth/login`,
-        method: 'POST',
-        body: { login, password },
-      }),
-    }),
-    // logout: builder.mutation({
-    //   query: () => ({
-    //     url: `/auth/logout`,
-    //     method: 'POST',
-    //     body: {},
-    //   }),
-    //   invalidatesTags: ['Post', 'Answer', 'Comment', 'Like'],
-    // }),
     deleteUser: builder.mutation({
       query: (userId) => ({
         url: `/users/${userId}`,
@@ -57,24 +35,8 @@ export const apiSlice = createApi({
         body: {},
       }),
     }),
-    resetPassword: builder.mutation({
-      query: ({ email }) => ({
-        url: `/auth/password-reset`,
-        method: 'POST',
-        body: { email },
-      }),
-    }),
-    confirmResetPassword: builder.mutation({
-      query: ({ token, newPassword }) => ({
-        url: `/auth/password-reset/${token}`,
-        method: 'POST',
-        body: { newPassword },
-      }),
-    }),
   }),
 });
-
-// ! add logout and invalidate all tags
 
 export const {
   useGetAuthorQuery,
@@ -83,9 +45,4 @@ export const {
   useUpdateUserAvatarMutation,
   useUpdateUserDataMutation,
   useDeleteUserMutation,
-  useResetPasswordMutation,
-  useConfirmResetPasswordMutation,
-  useRegisterMutation,
-  useLoginMutation,
-  // useLogoutMutation,
 } = apiSlice;
